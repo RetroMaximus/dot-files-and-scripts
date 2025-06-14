85db798d6f764cc910d3572cb996dc7bba0e73c3
RFPSystems Production Applications <59292329+RetroMaximus@users.noreply.github.com>
2025-06-14 18:40:55 -0400

    Update init_git.sh

--- init_git.sh
+++ init_git.sh
@@ -2,7 +2,7 @@ git init
 git add README.md
 git commit -m "Initial Commit"
 git branch -m main
-git remote add origin git@github.com:RetroMaximus/crudeirc.git
+git remote add origin git@github.com:GithubUser/project.git
 git push -u origin main
 
 # git remote add origim https://github.com/RetroMaximus/crudeird.git

1d2c2050cae080bb25bb25d2c4d35ccbcb681023
RFPSystems Production Applications <59292329+RetroMaximus@users.noreply.github.com>
2025-06-14 18:36:21 -0400

    Add files via upload

---
+++ init_git.sh
@@ -0,0 +1,10 @@
+git init
+git add README.md
+git commit -m "Initial Commit"
+git branch -m main
+git remote add origin git@github.com:RetroMaximus/crudeirc.git
+git push -u origin main
+
+# git remote add origim https://github.com/RetroMaximus/crudeird.git
+# git branch -m main
+# git push-u origin main

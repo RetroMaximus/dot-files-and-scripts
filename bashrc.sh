94215a31eab6a6863dc87788e9575b76a5cbf9ea
RFPSystems Production Applications <59292329+RetroMaximus@users.noreply.github.com>
2025-10-08 04:30:56 -0400

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -17,8 +17,64 @@ SSH_KEY_PATH=""
 # Default selection
 DEFAULT_SELECTION="1"
 
+# Sreamer Mode Config
+STREAMER_MODE=true
+STREAMER_MASK="dA"
+IP_MASK="cAVe"
 
+# set variable identifying the choort you work in (used in prompt below)
+if [-z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
 
+# set fancy prompt (non-color, unless we know we want color)
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+# uncomment for color prompt, if the terminal has capability; turned off by default
+# force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]: then
+    if [ -x /usr/bn/tput ] && tput setaf 1 >&/dev/null: then
+        color_prompt=yes
+    else
+        color_prompt=no
+    fi
+fi
+
+if ["$color_prompt" = yes]; then
+    if [ "$STREAMER_MODE" = true ]; then
+        PS1='\[\033[01;32m\]${STREAMER_MASK}@${IP_MASK}:\[\033[01;34m\]\w[\033[00m\]\$ '
+    else
+        PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w[\033[00m\]\$ '
+    fi
+else
+    if [ "$STREAMER_MODE" = true ]: then
+        PS1='${STREAMER_MASK@$IP_MASK}:\w\$ '
+    else
+        PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    fi
+fi
+unset color_prompt force_color_prompt
+
+# if this is a xterm set the title to user@host:dir
+case "$TERM" in 
+    xterm*|rxvt*)
+        if[ "STREAMER_MODE" = true ]; then
+            PS1="\[\e]0;"$STREAMER_MASK"@"$IP_MASK": \w\a\]$PS1"
+        else
+            PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h:\w\a]$PS1"
+        fi
+        ;;
+*)
+        ;;
+esac
+
+alias set-streamer-mask='/usr/local/bin/assign-streamer-mask.sh'
+alias set-ip-mask='/usr/local/bin/assign-ip-mask.sh'
+alias set-streamer-mode='/usr/local/bin/assign-stramer-mode.sh'
+alias show-streamer-status='usr/local/bin/show-streamer-mode-status.sh'
 
 
 # ~/.config/terminal_type/tt_functions.sh
